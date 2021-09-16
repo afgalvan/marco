e0b0dcf23fd5d00c36cc89ccc27107d9626e73e7
Andres <afgalvan@unicesar.edu.co>
2021-09-16 18:11:06 +0100

    docs: add 2 examples for different variables

--- examples/quantitative_example.py
+++ examples/quantitative_example.py
@@ -1,20 +1,30 @@
-import os
-import sys
-import matplotlib.pyplot as plt
+# import os
+# import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+# sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from marco import QuantitativeFrame, quantitative_table
 
-data = [170, 172, 183, 164 ,166, 164, 160, 168, 173, 176,
+# Grouped data ----------------------------------------------------------
+grouped_data = [170, 172, 183, 164 ,166, 164, 160, 168, 173, 176,
 184, 190, 163, 151, 158, 166 ,166, 172, 170, 186,
 170, 166, 169, 160, 158, 155 ,160, 157, 163, 165,
 170, 172, 178, 183, 170, 166 ,165, 172, 168, 158,
 158, 168, 166, 159, 154, 166 ,172, 168, 163, 172]
 
+quantitative_frame: QuantitativeFrame = quantitative_table(grouped_data)
 
-data2 = [4, 5, 6, 7, 4, 5, 7, 7, 5, 7, 1, 3, 4, 5, 6, 3, 5, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 6]
-quantitative_frame: QuantitativeFrame = quantitative_table(data2)
+print(quantitative_frame.dataframe)
+print("\n\nmedia aritmética: ", quantitative_frame.arithmetic_mean())
+print("moda: {:.2f}".format(quantitative_frame.trend()))
+print("fila: ")
+print(quantitative_frame.trend_row())
+print("mediana: {:.2f}".format(quantitative_frame.median()))
+print("fila: ")
+print(quantitative_frame.median_row())
 
+# Ungrouped --------------------------------------------------------------
+ungrouped_data = [4, 5, 6, 5, 1, 3, 4, 5, 6, 3, 5, 3, 3, 3]
+quantitative_frame: QuantitativeFrame = quantitative_table(ungrouped_data)
 print("Ta bli ta")
 print(quantitative_frame.dataframe)
 print("\nMedia arigmedica")
